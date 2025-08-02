acc031988fa1b0dd0c8b16fcc8f8154c6206c078
Alexandre 'pocky' Balmes <github@alexandre.balmes.co>
2025-08-02 22:03:39 +0200

    feat(ecs): Updat ecs configuration

--- symplify/easy-coding-standard/12.0/ecs.php
+++ symplify/easy-coding-standard/12.0/ecs.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Basic\PsrAutoloadingFixer;
 use PhpCsFixer\Fixer\CastNotation\NoUnsetCastFixer;
-use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
-use PhpCsFixer\Fixer\FunctionNotation\NullableTypeDeclarationForDefaultNullValueFixer;
 use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
@@ -25,24 +23,14 @@ use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
 use PhpCsFixer\Fixer\StringNotation\SimpleToComplexStringVariableFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(SetList::COMMON);
-    $ecsConfig->import(SetList::PSR_12);
-    $ecsConfig->import(SetList::CLEAN_CODE);
-    $ecsConfig->import(SetList::ARRAY);
-    $ecsConfig->dynamicSets(['@Symfony', '@PHP84Migration']);
-
-    $ecsConfig->cacheDirectory('.ecs_cache');
-    $ecsConfig->parallel();
-
-    $ecsConfig->paths([
-        __DIR__.'/src',
-        __DIR__.'/tests',
-    ]);
-
-    $ecsConfig->rules([
+return ECSConfig::configure()
+    ->withPaths([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->withParallel()
+    ->withCache('.ecs_cache')
+    ->withEditorConfig()
+    ->withRootFiles()
+    ->withRules([
         DeclareStrictTypesFixer::class,
         OrderedImportsFixer::class,
         NoUnusedImportsFixer::class,
@@ -51,15 +39,30 @@ return static function (ECSConfig $ecsConfig): void {
         NoUnsetCastFixer::class,
         SimpleToComplexStringVariableFixer::class,
         PsrAutoloadingFixer::class,
-    ]);
-
-    $ecsConfig->ruleWithConfiguration(YodaStyleFixer::class, [
-        'equal' => true,
-        'identical' => true,
-        'less_and_greater' => true,
-    ]);
-
-    $ecsConfig->ruleWithConfiguration(PhpdocAlignFixer::class, [
+        FullyQualifiedStrictTypesFixer::class,
+    ])
+    ->withPreparedSets(
+        psr12: true,
+        common: true,
+        cleanCode: true,
+    )
+    ->withSkip([
+        __DIR__ . '/src/Shared/Infrastructure/Persistence/Doctrine/ORMRepository.php',
+        PhpUnitTestClassRequiresCoversFixer::class => ['*Test.php'],
+        PhpUnitInternalClassFixer::class => ['*Test.php'],
+        PhpUnitMethodCasingFixer::class => ['*Test.php'],
+        NotOperatorWithSuccessorSpaceFixer::class,
+        PhpdocToCommentFixer::class,
+        PhpdocSummaryFixer::class,
+        PhpdocLineSpanFixer::class,
+        NoBlankLinesAfterPhpdocFixer::class,
+    ])
+    ->withConfiguredRule(
+        NullableTypeDeclarationFixer::class, [
+            'syntax' => 'union',
+        ]
+    )
+    ->withConfiguredRule(PhpdocAlignFixer::class, [
         'align' => 'left',
         'tags' => [
             'method',
@@ -70,20 +73,14 @@ return static function (ECSConfig $ecsConfig): void {
             'type',
             'var',
         ],
-    ]);
-
-    $ecsConfig->ruleWithConfiguration(NullableTypeDeclarationFixer::class, [
-        'syntax' => 'union',
-    ]);
-
-    $ecsConfig->skip([
-        PhpUnitTestClassRequiresCoversFixer::class => ['*Test.php'],
-        PhpUnitInternalClassFixer::class => ['*Test.php'],
-        PhpUnitMethodCasingFixer::class => ['*Test.php'],
-        NotOperatorWithSuccessorSpaceFixer::class,
-        PhpdocToCommentFixer::class,
-        PhpdocSummaryFixer::class,
-        PhpdocLineSpanFixer::class,
-        NoBlankLinesAfterPhpdocFixer::class,
-    ]);
-};
+    ])
+    ->withConfiguredRule(YodaStyleFixer::class, [
+        'equal' => true,
+        'identical' => true,
+        'less_and_greater' => true,
+    ])
+    ->withPhpCsFixerSets(
+        symfony: true,
+        php84Migration: true,
+    )
+;
